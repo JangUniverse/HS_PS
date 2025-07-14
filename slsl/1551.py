a39c702c42436451882e34a86244fb690534c097
djsiui12 <bouncelee2008@gmail.com>
2025-07-14 19:23:08 +0900

    Update 1551.py

--- slsl/1551.py
+++ slsl/1551.py
@@ -1,11 +1,7 @@
-n=int(input())
-A=input().split()
-k=int(input())
-for i in range(n):
-    if k==int(A[i]):
-        print(i+1)
-        x=False
-        break
-    x=True
-if x:
-    print(-1)
+a,b=input().split()
+a=int(a)
+b=int(b)
+if a<b:
+    print(a,b)
+else:
+    print(b,a)
