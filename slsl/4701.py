97894af11bf168c5680f810bb9c14bda1c5ce695
djsiui12 <bouncelee2008@gmail.com>
2025-07-16 14:20:42 +0900

    chy

--- slsl/4701.py
+++ slsl/4701.py
@@ -1,1 +1,8 @@
-print('개어렵네')
+n=int(input())
+A=list(map(int,input().split()))
+A.sort(key=lambda x: abs(x))
+k=(abs(A[0]+A[1]),0,1)
+for i in range(n-1):
+    if k[0]>abs(A[i]+A[i+1]):
+        k=(abs(A[i]+A[i+1]),i,i+1)
+print(A[k[1]],A[k[2]])
