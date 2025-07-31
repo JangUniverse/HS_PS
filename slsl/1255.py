a8d40f5f246c1af9b2f117d2b32c7ccc58e3e281
djsiui12 <bouncelee2008@gmail.com>
2025-07-31 15:07:08 +0900

    j

--- slsl/1255.py
+++ slsl/1255.py
@@ -1,6 +1,6 @@
-# a,b=map(float,input().split())
-# A=int(a*100)
-# B=int(b*100)
-# for i in range(A,B+1):
-#     print(format(i/100,".2f"),end=" ")
-#w같은거 모르겟음.
+a, b = map(float, input().split())
+while True:
+    if a > b:
+        break
+    print(f"{a:.2f}", end = ' ')
+    a = a+0.01
